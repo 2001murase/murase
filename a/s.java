18eb3cf0a71dce8c7e77a3875a84b65172df0845
2001murase <b320.murase2022tec@gmail.com>
2023-01-19 13:36:17 +0900

    test

---
+++ a/s.java
@@ -0,0 +1,21 @@
+//import com.amazonaws.AmazonServiceException;
+//import com.amazonaws.services.dynamodbv2.*;
+/*import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
+import java.util.HashMap;
+import java.util.Map;*/
+import java.lang.*;
+
+public class s {
+    public static void main(String[] args){
+        //AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_WEST_2).build(); 
+       
+    }
+}
+
+class TestClass {
+        public void testClass() {
+           
+        }
+    }
